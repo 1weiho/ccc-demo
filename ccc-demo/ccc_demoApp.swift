1f60591133f57bce6fed5338b903fe4493d37d88
David Ho <davidho0403@gmail.com>
2023-06-03 01:28:16 +0800

    Initial Commit

---
+++ ccc-demo/ccc_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ccc_demoApp.swift
+//  ccc-demo
+//
+//  Created by 何奕瑋 on 2023/6/3.
+//
+
+import SwiftUI
+
+@main
+struct ccc_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
